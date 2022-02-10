86a3836cdc174a9a414268c6a381c40686a03ba0
Bhushan Borkar <bhushan.borkar@kyndryl.com>
2022-02-10 14:38:07 +0530

    added a datatable resource

--- src/main/java/com/github/bhborkar/qlearning/api/PersonResource.java
+++ src/main/java/com/github/bhborkar/qlearning/api/PersonResource.java
@@ -7,10 +7,12 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import com.github.bhborkar.qlearning.entities.Person;
 import com.github.bhborkar.qlearning.enums.EyeColor;
+import com.github.bhborkar.qlearning.model.DataTable;
 
 @Path("/person")
 @ApplicationScoped
@@ -28,4 +30,20 @@ public class PersonResource {
     public List<Person> getByColor(@PathParam("color") EyeColor color) {
         return Person.findByColor(color);
     }
+    
+    @GET
+    @Path("/datatable")
+    @Produces(MediaType.APPLICATION_JSON)
+    public DataTable<Person> datatable(
+        @QueryParam(value = "draw") int draw,
+        @QueryParam(value = "start") int start,
+        @QueryParam(value = "length") int length,
+        @QueryParam(value = "search[value]") String searchVal
+
+        ) {
+            DataTable<Person> datatable = new DataTable<>();
+            datatable.setDraw(draw);
+            return datatable;
+    }
+    
 }
